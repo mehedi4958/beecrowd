0085c7679cece2786d260ac68c8bc00504bd26e8
Mehedi <mehedi@augnitive.com>
2022-09-30 12:56:26 +0600

    beecrwod 1143 accepted

---
+++ beecrowd_1143_squared_and_cubic/test/beecrowd_1143_squared_and_cubic_test.dart
@@ -0,0 +1,8 @@
+import 'package:beecrowd_1143_squared_and_cubic/beecrowd_1143_squared_and_cubic.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
