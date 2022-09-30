535a9c02547bbfbb452653c367628b65bc944fbd
Mehedi <mehedi@augnitive.com>
2022-09-30 10:32:03 +0600

    beecrowd 1118 accepted

---
+++ beecrowd_1118_several_scores_with_validation/lib/beecrowd_1118_several_scores_with_validation.dart
@@ -0,0 +1,6 @@
+/*
+ * Solution in in the bin folder
+*/
+int calculate() {
+  return 6 * 7;
+}
---
+++ beecrowd_1118_several_scores_with_validation/test/beecrowd_1118_several_scores_with_validation_test.dart
@@ -0,0 +1,8 @@
+import 'package:beecrowd_1118_several_scores_with_validation/beecrowd_1118_several_scores_with_validation.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
