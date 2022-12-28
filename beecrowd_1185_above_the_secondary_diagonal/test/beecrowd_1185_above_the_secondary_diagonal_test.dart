fc13d82a5f26bf18c198fced882b93f2e85f8daf
Mehedi <mehedi@augnitive.com>
2022-12-28 22:59:53 +0600

    beecrowd 1185 accepted

---
+++ beecrowd_1185_above_the_secondary_diagonal/test/beecrowd_1185_above_the_secondary_diagonal_test.dart
@@ -0,0 +1,8 @@
+// import 'package:beecrowd_1185_above_the_secondary_diagonal/beecrowd_1185_above_the_secondary_diagonal.dart';
+// import 'package:test/test.dart';
+
+// void main() {
+//   test('calculate', () {
+//     expect(calculate(), 42);
+//   });
+// }
