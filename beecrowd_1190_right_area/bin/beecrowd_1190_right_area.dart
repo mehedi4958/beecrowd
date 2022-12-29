f9180cd2cb83266dd6986d4ace743d09d81a2b55
Mehedi <mehedi@augnitive.com>
2022-12-29 10:39:57 +0600

    beecrowd 1190 accepted

---
+++ beecrowd_1190_right_area/bin/beecrowd_1190_right_area.dart
@@ -0,0 +1,25 @@
+import 'dart:io';
+
+void main() {
+  final list = List.generate(12, (index) => List<double>.filled(12, 0));
+
+  String o = stdin.readLineSync()!;
+
+  double sum = 0;
+  int count = 0;
+  for (int i = 0; i < list.length; i++) {
+    for (int j = 0; j < list.length; j++) {
+      list[i][j] = double.parse(stdin.readLineSync()!);
+      if (j > i && i+j > 11) {
+        sum += list[i][j];
+        count++;
+      }
+    }
+  }
+
+  if (o == 'S') {
+    print(sum.toStringAsFixed(1));
+  } else {
+    print((sum / count).toStringAsFixed(1));
+  }
+}
---
+++ beecrowd_1190_right_area/test/beecrowd_1190_right_area_test.dart
@@ -0,0 +1,8 @@
+// import 'package:beecrowd_1190_right_area/beecrowd_1190_right_area.dart';
+// import 'package:test/test.dart';
+
+// void main() {
+//   test('calculate', () {
+//     expect(calculate(), 42);
+//   });
+// }
