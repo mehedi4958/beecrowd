3e070ac918455a4e88546ca5168828cea690fbae
Mehedi <mehedi@augnitive.com>
2022-12-29 10:29:24 +0600

    beecrowd 1188 accepted

---
+++ beecrowd_1188_inferior_area/test/beecrowd_1188_inferior_area_test.dart
@@ -0,0 +1,8 @@
+// import 'package:beecrowd_1188_inferior_area/beecrowd_1188_inferior_area.dart';
+// import 'package:test/test.dart';
+
+// void main() {
+//   test('calculate', () {
+//     expect(calculate(), 42);
+//   });
+// }
